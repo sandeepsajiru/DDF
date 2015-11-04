0c2dd818466cfcd67194f4da1c9ae3f3ee79d2fa
Sandeep Kumar <beingzeroin@gmail.com>
2015-11-05 00:49:09 +0530

    Added SelectionHelper and It's Unit Tests

---
+++ Module-20/DataDrivenFramework/SelectionHelper.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DatadrivenFramework;
+
+namespace DataDrivenFramework
+{
+    public class SelectionHelper
+    {
+        public static bool IsTestSelected(String suiteFilePath, String testName)
+        {
+            ExcelHelper eh = new ExcelHelper(suiteFilePath);
+            int testCaseRowNumber = eh.GetRowNumber("TestCases", "TestCases", testName);
+            String runModeValue = eh.GetCellData("TestCases", "Runmode", testCaseRowNumber);
+
+            // APPROACH 2
+            return runModeValue.Equals("Y", StringComparison.OrdinalIgnoreCase);
+
+        }
+        public static bool IsSuiteSelected(String suitesFilePath, String suiteName)
+        {
+            ExcelHelper eh  = new ExcelHelper(suitesFilePath);
+            int suiteRowNumber = eh.GetRowNumber("Suite", "SuiteName", suiteName);
+            String runModeValue = eh.GetCellData("Suite", "Runmode", suiteRowNumber);
+
+            // APPROACH 1
+            if (runModeValue.Equals("Y", StringComparison.OrdinalIgnoreCase))
+                return true;
+            return false;
+        }
+    }
+}
---
+++ Module-20/TestAutomation/UnitTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using NUnit.Framework;
+using DataDrivenFramework;
+
+namespace TestAutomation
+{
+    [TestFixture]
+    public class UnitTests
+    {
+        [Test]
+        public void SuiteSelectionTest()
+        {
+            Console.WriteLine(SelectionHelper.IsSuiteSelected("TestData\\Suite.xlsx", "SuiteA"));
+            Console.WriteLine(SelectionHelper.IsSuiteSelected("TestData\\Suite.xlsx", "SuiteB"));
+            Console.WriteLine(SelectionHelper.IsSuiteSelected("TestData\\Suite.xlsx", "SuiteC"));
+        }
+
+        [Test]
+        public void TestSelectionTest()
+        {
+            Console.WriteLine(SelectionHelper.IsTestSelected("TestData\\SuiteA.xlsx", "Test1"));
+            Console.WriteLine(SelectionHelper.IsTestSelected("TestData\\SuiteA.xlsx", "Test2"));
+            Console.WriteLine(SelectionHelper.IsTestSelected("TestData\\SuiteA.xlsx", "Test3"));
+        }
+    }
+}
