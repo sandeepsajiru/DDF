bacbfca6be52cfac16418c36e37f71560ecf19c2
Sandeep <sandeep.sajiru@gmail.com>
2015-11-23 22:47:52 +0530

    Added AppleTest

---
+++ Module-20/TestAutomation/AppleTest.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.IE;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TestAutomation
+{
+    [TestFixture]
+    [Parallelizable(ParallelScope.Self)]
+    class AppleSeleniumTest : TestCaseBase
+    {
+        [Test, TestCaseSource(typeof(TestCaseBase), "BrowserData")]
+        public void AppleTest(String browserName)
+        {
+            RunTest(browserName, "http://apple.com");
+        }
+
+    }
+}
